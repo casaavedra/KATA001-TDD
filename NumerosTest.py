ce2d54c99f21820ccd5ff81a190619a87a3839db
Carlos Saavedra <ca.saavedra@uniandes.edu.co>
2016-09-21 20:24:46 -0500

    Iteración 1 - Paso 2 - Ciclo TDD para que funcione con un string de 1 número (red)

--- NumerosTest.py
+++ NumerosTest.py
@@ -5,3 +5,6 @@ from Numeros import Numeros
 class NumerosTest(TestCase):
     def test_numElementos(self):
         self.assertEqual(Numeros().numElementos(""),[0],"Cadena vacía")
+
+    def test_numElementos_1(self):
+        self.assertEqual(Numeros().numElementos("8"), [1], "Un elemento")
